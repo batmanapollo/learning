ccbe8d6d813d63626ea71c57452ccaefee8cd15c
batmanapollo <stepan-1@hotmail.com>
2022-10-16 16:31:12 +0700

    Update EvenNumbers.java

--- recursion/EvenNumbers.java
+++ recursion/EvenNumbers.java
@@ -5,15 +5,19 @@ import java.util.List;
 public class EvenNumbers {
 
     public void printEvenNumbers(List<Integer> numbers) {
-        if (numbers.size() == 0) {
+        printEvenNumbers(numbers, 0);
+    }
+
+    private void printEvenNumbers(List<Integer> numbers, int index) {
+        if (numbers.size() == 0 || index == numbers.size()) {
             return;
         }
-        var number = numbers.get(0);
+        var number = numbers.get(index);
         var shouldPrintNumber = number % 2 == 0;
         if (shouldPrintNumber) {
             System.out.println(number);
         }
-        printEvenNumbers(numbers.subList(1, numbers.size()));
+        printEvenNumbers(numbers, index + 1);
     }
 
 }
