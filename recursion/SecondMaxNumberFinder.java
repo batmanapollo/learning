1277e3a32c87b6baec27e25986cdfadf85e6eba4
batmanapollo <stepan-1@hotmail.com>
2022-10-17 17:19:32 +0700

    Create SecondMaxNumberFinder.java

---
+++ recursion/SecondMaxNumberFinder.java
@@ -0,0 +1,33 @@
+package recursion;
+
+import java.util.List;
+
+public class SecondMaxNumberFinder {
+
+    public int find(List<Integer> numbers) {
+        int first = numbers.get(0);
+        int second = numbers.get(1);
+        if (first > second) {
+            return find(numbers, 0, first, second);
+        } else {
+            return find(numbers, 0, second, first);
+        }
+    }
+
+    private int find(List<Integer> numbers, int index, int max, int secondMax) {
+        if (index == numbers.size()) {
+            return secondMax;
+        }
+
+        var number = numbers.get(index);
+        if (number > max) {
+            secondMax = max;
+            max = number;
+        } else if (number < max && number > secondMax) {
+            secondMax = number;
+        }
+
+        return find(numbers, index + 1, max, secondMax);
+    }
+
+}
